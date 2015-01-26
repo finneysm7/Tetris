c48694561d2de1d3d48551e4472d6a76fcd91211
Sean Finney <mcfinneys@gmail.com>
2015-01-26 01:16:48 -0800

    Fixed rod and block tests

--- spec/block_spec.rb
+++ spec/block_spec.rb
@@ -6,6 +6,29 @@ describe "Block" do
   let(:board) {Board.new}
   let(:block) {Block.new(board)}
   
+  it "should have a board" do
+    expect(block.board).to eq(board)
+    expect(board[[block.squares[0].pos[0],block.squares[0].pos[1]]]).to eq(block.squares[0])
+  end
+  
+  it "should have the correct number of squares" do
+    expect(block.squares.length).to eq(4)
+  end
+  
+  it "should start in the middle of the board" do
+    expect(block.squares[0].pos).to eq([1, 4])
+    expect(block.squares[1].pos).to eq([0, 4])
+    expect(block.squares[2].pos).to eq([0, 5])
+    expect(block.squares[3].pos).to eq([1, 5])
+  end
+  
+  it "should exist on the board" do
+    expect(block.board[[1,4]]).to eq(block.squares[0])
+    expect(block.board[[0,4]]).to eq(block.squares[1])
+    expect(block.board[[0,5]]).to eq(block.squares[2])
+    expect(block.board[[1,5]]).to eq(block.squares[3])
+  end
+  
   context "#down" do
     before(:each) do
       block.down
--- spec/rod_spec.rb
+++ spec/rod_spec.rb
@@ -1,12 +1,33 @@
 require 'rspec'
 require 'piece'
+require 'board'
 require 'rod'
 
 
 describe "rod" do
-  let(:rod) {Rod.new}
+  let(:board) {Board.new}
+  let(:rod) {Rod.new(board)}
   
-  it "makes a rod" do
-    
+  it "should have a board" do
+    expect(rod.board).to eq(board)
+    expect(board[[rod.squares[0].pos[0],rod.squares[0].pos[1]]]).to eq(rod.squares[0])
+  end
+  
+  it "should have the correct number of squares" do
+    expect(rod.squares.length).to eq(4)
+  end
+  
+  it "should start in the middle of the board" do
+    expect(rod.squares[0].pos).to eq([0, 3])
+    expect(rod.squares[1].pos).to eq([0, 4])
+    expect(rod.squares[2].pos).to eq([0, 5])
+    expect(rod.squares[3].pos).to eq([0, 6])
+  end
+  
+  it "should exist on the board" do
+    expect(rod.board[[0,3]]).to eq(rod.squares[0])
+    expect(rod.board[[0,4]]).to eq(rod.squares[1])
+    expect(rod.board[[0,5]]).to eq(rod.squares[2])
+    expect(rod.board[[0,6]]).to eq(rod.squares[3])
   end
 end
